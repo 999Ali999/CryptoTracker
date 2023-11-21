4b13e0300a0c5a2a560acc81ab2bc1427dd63521
JuiceWRLD <127804196+999Ali999@users.noreply.github.com>
2023-11-21 16:23:53 +0100

    init commit

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Ali on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
